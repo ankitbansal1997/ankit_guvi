e61fd3de2e018bd747cccac884ec9849a63ac875
ankitbansal1997 <btpankit1997@gmail.com>
2019-01-14 22:29:46 +0530

    Create ankit2.c

---
+++ ankit2.c
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin >> n;
+	if(n<0){
+		cout << "invalid" << endl;
+	}
+	if(n>0){
+	
+	if(n%2==0)
+	{
+		cout << "Even" << endl;
+	}
+	else {
+		cout << "Odd" << endl;
+	}
+}
+}
