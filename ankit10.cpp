eaccf52257fed8f19d814ed1ceb69d17ea93919f
ankitbansal1997 <btpankit1997@gmail.com>
2019-01-15 18:23:44 +0530

    Create ankit10.cpp

---
+++ ankit10.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+	int i,x,n,a,c=0,sum=0;
+	cin >> n;
+	x=n;
+	while(x!=0){
+		x=x/10;
+		c++;
+	}
+x=n;
+while(x!=0){
+	a=x%10;
+	sum=sum*10+a;
+	x=x/10;
+}
+if(n==sum)
+cout << "yes";
+else 
+cout << "no";
+
+}
