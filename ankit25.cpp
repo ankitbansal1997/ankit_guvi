53443d470e85a7e4da108aa7a089238fcaf80efc
ankitbansal1997 <btpankit1997@gmail.com>
2019-01-17 21:55:35 +0530

    Create ankit25.cpp

---
+++ ankit25.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+	int i,n1,n2,m1,m2;
+	cin >> n1 >> m1>> n2>>m2;
+	int x=abs(n1-n2);
+	int y=abs(m1-m2);
+		cout << x << " " <<  y << endl;
+	
+}
